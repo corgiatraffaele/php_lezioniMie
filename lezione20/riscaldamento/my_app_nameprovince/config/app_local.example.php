d486899686eb741d47599ba7fa5e747a7e773157
Raf <raffele.corgiat@gmail.com>
2022-04-28 13:12:06 +0200

    lezione21

--- lezione20/riscaldamento/my_app_nameprovince/config/app_local.example.php
+++ lezione20/riscaldamento/my_app_nameprovince/config/app_local.example.php
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
-            'password' => 'secret',
+            'username' => 'lezione20',
+            'password' => 'lezione20',
 
-            'database' => 'my_app',
+            'database' => 'lezione20',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
